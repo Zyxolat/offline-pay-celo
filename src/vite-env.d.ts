4dd70445ce75ad854df3c19a38811ffbc94e3e41
zyxolat <zyxolat@gmail.com>
2026-04-16 20:24:06 +0100

    chore: add window.ethereum typings for MetaMask support

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface EthereumRequestArguments {
+  method: string;
+  params?: unknown[] | Record<string, unknown>;
+}
+
+interface EthereumProvider {
+  request: (args: EthereumRequestArguments) => Promise<unknown>;
+}
+
+interface Window {
+  ethereum?: EthereumProvider;
+}
